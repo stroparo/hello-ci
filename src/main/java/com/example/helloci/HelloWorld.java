3b57153fef5e09a122989428281e56b3f9a15d4c
Cristian Stroparo <cristian.stroparo@mastercard.com>
2018-10-02 21:21:21 -0300

    Update port

--- src/main/java/com/example/helloci/HelloWorld.java
+++ src/main/java/com/example/helloci/HelloWorld.java
@@ -2,7 +2,7 @@ import static spark.Spark.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        port(8421);
+        port(8420);
         get("/", (req, res) -> "Hello World");
     }
 }
