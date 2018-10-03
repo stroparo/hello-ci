f14d960d2cf12880ae02257aed8b82a9ba214316
Cristian Stroparo <cristian.stroparo@mastercard.com>
2018-10-03 01:54:05 -0300

    Update hello message according to requirements

--- src/main/java/com/example/helloci/HelloWorld.java
+++ src/main/java/com/example/helloci/HelloWorld.java
@@ -3,6 +3,6 @@ import static spark.Spark.*;
 public class HelloWorld {
     public static void main(String[] args) {
         port(4321);
-        get("/", (req, res) -> "Hello World");
+        get("/", (req, res) -> "Hello from foobar");
     }
 }
